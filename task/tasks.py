9d0d33ec25ffd4b0931c558153621249327e9eb3
张保良 <jfreebird@gmail.com>
2012-04-06 18:40:08 +0800

    添加更新任务数据的CELERY任务

--- task/tasks.py
+++ task/tasks.py
@@ -1,6 +1,10 @@
 from service.task import convert_media 
 from celery.decorators import task
-
+from service.task import update_task_data_media
 @task    
 def convert_task():
     convert_media()
+
+@task
+def update_task(): 
+    update_task_data_media() 
